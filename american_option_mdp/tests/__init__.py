5b52d90f123694087007628d1116df937492297f
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2026-01-14 17:44:44 +0800

    Add files via upload

--- american_option_mdp/tests/__init__.py
+++ american_option_mdp/tests/__init__.py
@@ -1,1 +1,21 @@
+from .pricers import american_option_mdp, american_option_mdp_stoch_vol
+from .utils import (
+    compute_greeks_mdp,
+    calibrate_implied_volatility,
+    plot_exercise_boundary,
+    plot_value_function,
+    plot_policy_map,
+    save_results_to_csv
+)
 
+__version__ = "1.0.0"
+__all__ = [
+    "american_option_mdp",
+    "american_option_mdp_stoch_vol",
+    "compute_greeks_mdp",
+    "calibrate_implied_volatility",
+    "plot_exercise_boundary",
+    "plot_value_function",
+    "plot_policy_map",
+    "save_results_to_csv"
+]
